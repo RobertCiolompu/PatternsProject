b94b6deee35befda233967d7e120f0e1d01bab17
RobertCiolompu <52103599+RobertCiolompu@users.noreply.github.com>
2019-07-20 00:16:27 +0300

    Update of Specialization Class
    Changed way off calling FirstTalentSet and SecondTalentSet methods due to their update.

--- PatternsProject/Specialization.cs
+++ PatternsProject/Specialization.cs
@@ -10,33 +10,32 @@ namespace PatternsProject
     {
         FirstTalentSet firstTalentSet = new FirstTalentSet();
         SecondTalentSet secondTalentSet = new SecondTalentSet();
-        private string retribution = "";
-        private string holy = "";
-        private string arms = "";
-        private string fury = "";
-        private string frost = "";
-        private string fire = "";
-        
 
         protected void getPaladinInformation()
         {
-            firstTalentSet.GetRetribution(retribution);
-            secondTalentSet.GetHoly(holy);
-            Console.WriteLine("Retribution Specialization:\n{0}\n\nHoly Specialization\n{1}",retribution,holy);
+            Console.WriteLine("Retribution Specialization:");
+            firstTalentSet.GetRetribution();
+            Console.WriteLine("\nHoly Specialization:");
+            secondTalentSet.GetHoly();
+            
         }
 
         protected void getWarriorInformation()
         {
-            firstTalentSet.GetArms(arms);
-            secondTalentSet.GetFury(fury);
-            Console.WriteLine("Arms Specialization:\n{0}\n\nFury Specialization\n{1}", arms, fury);
+            Console.WriteLine("Arms Specialization:");
+            firstTalentSet.GetArms();
+            Console.WriteLine("\nFury Specialization:");
+            secondTalentSet.GetFury();
+            
         }
 
         protected void getMageInformation()
         {
-            firstTalentSet.GetFrost(frost);
-            secondTalentSet.GetFire(fire);
-            Console.WriteLine("Frost Specialization:\n{0}\n\nFire Specialization\n{1}", frost, fire);
+            Console.WriteLine("Frost Specialization:");
+            firstTalentSet.GetFrost();
+            Console.WriteLine("\nFire Specialization:");
+            secondTalentSet.GetFire();
+            
         }
     }
 }
